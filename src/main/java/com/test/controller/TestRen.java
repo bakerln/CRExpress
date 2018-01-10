d3512fa4e4dae701fa7846721c77ae435a5471ff
renxinliang <451757560@qq.com>
2018-01-10 09:57:28 +0800

    from ren

--- src/main/java/com/test/controller/TestRen.java
+++ src/main/java/com/test/controller/TestRen.java
@@ -1,5 +1,21 @@
 package com.test.controller;
 
+import java.util.Scanner;
+
 public class TestRen {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner (System.in);
+           System.out.println("请输入数字");
+        double i=scanner.nextDouble();
+        if(i<10)
+            System.out.println(i + "小于10");
+      else if(i>10)
+          System.out.println(i + " 大于10");
+      else
+          System.out.println(i +"等于10");
+      //判断语句  if else
+    }
+
 
 }
