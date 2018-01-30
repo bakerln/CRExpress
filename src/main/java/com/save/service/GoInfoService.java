fbb0ec38d89cb62a1757445b5484e9c2aef097f9
renxinliang <451757560@qq.com>
2018-01-30 09:41:15 +0800

    from ren

--- src/main/java/com/save/service/GoInfoService.java
+++ src/main/java/com/save/service/GoInfoService.java
@@ -1,14 +1,13 @@
 package com.save.service;
 
+import com.common.util.json.ResultMsg;
 import com.common.util.session.UserSession;
 import com.save.dao.GoInfoDao;
 import com.save.model.GoInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.Date;
-import java.sql.Time;
-import java.sql.Timestamp;
+import java.util.Date;
 
 /**
  * Created by linan on 2018-01-04.
@@ -21,14 +20,18 @@ public class GoInfoService {
     private GoInfoDao goInfoDao;
 
     //新增信息
-        public int add(GoInfo goInfo,UserSession userSession) {
-        int goInfoID = goInfoDao.createGOInfoId();
-        goInfo.setId(goInfoID);//添加信息ID
-        goInfo.setCreatTime(new java.util.Date());//添加创建时间
-        goInfo.setUserID(userSession.getUserId());//添加创建用户ID
-        return goInfoDao.add(goInfo);
+        public ResultMsg add(GoInfo goInfo, UserSession userSession) {
+            int goInfoID = goInfoDao.createGOInfoId();
+            goInfo.setId(goInfoID);//添加信息ID
+            goInfo.setCreatTime(new Date());//添加创建时间
+            goInfo.setUserID(userSession.getUserId());//添加创建用户ID
+
+            goInfoDao.add(goInfo);
+            return null;
+
 
     }
 
-    }
+
+}
 
