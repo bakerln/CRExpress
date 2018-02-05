afd29f9932083eef2e6e2540a8b1106a690593ec
renxinliang <451757560@qq.com>
2018-02-05 11:00:50 +0800

    from ren

--- src/main/java/com/save/dao/GoInfoDao.java
+++ src/main/java/com/save/dao/GoInfoDao.java
@@ -22,15 +22,15 @@ public class GoInfoDao {
     }
 
     public int add(GoInfo goInfo) {
-        String sql = "INSERT INTO SYS_GOINFO (ID,FROMSTATION,TRAINNUMBER,FROMDATE,EXITPORTSTATION,OVERSESASSTATION,OVERSEASCOUNTRY,OVERSEASCITY,TRAINSTATE,ORGID,USERID,CREATTIME,TRAINQTY,CARRIAGEQTY,HEAVYQTYTWENTY,EMPTYQTYTWENTY,HEAVYQTYFORTY,EMPTYQTYFORTY,HEAVYQTYFORTYFIVE,EMPTYQTYFORTYFIVE,TEU,COLDTEU,COLDWEIGHT,REMARK,ISDELETE,TYPE) " +
-                "VALUES(:id,fromStation,tiranNumber,fromData,exitPortStation,overseasStation,overseasCountry,overseasCity,trainState,orgID,userID,creatTime,trainQty,carriageQty,heavyQtyTwenty,emptyQtyTwenty,heavyQtyForty,emptyQtyForty,heavyQtyFortyfive,emptyFortyfive,TEU,coldTEU,coldWeight,remark,isDelete,type)";
+        String sql = "INSERT INTO SYS_GOINFO (ID,FROMSTATION,TRAINNUMBER,FROMDATE,EXITPORTSTATION,OVERSEASSTATION,OVERSEASCOUNTRY,OVERSEASCITY,TRAINSTATE,ORGID,USERID,CREATETIME,TRAINQTY,CARRIAGEQTY,HEAVYQTYTWENTY,EMPTYQTYTWENTY,HEAVYQTYFORTY,EMPTYQTYFORTY,HEAVYQTYFORTYFIVE,EMPTYQTYFORTYFIVE,TEU,COLDTEU,COLDWEIGHT,REMARK,ISDELETE,SAVETYPE) " +
+                "VALUES(:id,fromStation,tiranNumber,fromDate,exitPortStation,overseasStation,overseasCountry,overseasCity,trainState,orgID,userID,createTime,trainQty,carriageQty,HeavyQtyTwenty,EmptyQtyTwenty,HeavyQtyForty,EmptyQtyForty,HeavyQtyFortyfive,EmptyFortyfive,TEU,coldTEU,coldWeight,remark,isDelete,saveType)";
         NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
         SqlParameterSource paramSource = new BeanPropertySqlParameterSource(goInfo);
         return namedParameterJdbcTemplate.update(sql, paramSource);
     }
 
     public int update(GoInfo goInfo){
-        String sql = "UPDATE SYS_GOINFO SET FROMSTATION=:fromStation,TRAINNUMBER=:trainNumber,FROMDATE=:fromDate,EXITPORTSTATION=:exitPortStation,OVERSEASSTATION=:overseasStation,OVERSEASCOUNTRY=:overseasCountry,OVERSEASCITY=:overseasCity,TRAINSTATE:=trainState,UPDATETIME=:updateTime,TRAINQTY=:trainQty,CARRIAGEQTY=:carriageQty,HEAVYQTYTWENTY=:heavyQtyTwenty,EMPTYQTYTWENTY=:emptyQtyTwenty,HEAVYQTYforty=:heavyQtyForty,EMPTYQTYFORTY=:emptyQtyForty,HEAVYQTYFORTYFIVE=:heavyQtyFortyfive,EMPTYQTYFORTYFIVE=:emptyQtyFortyfive,TEU=:TEU,COLDTEU=:coldTEU,COLDWEIGHT=:coldWeight,REMARK=:remark,ISDELETE=:isDelete,TYPE=:type";
+        String sql = "UPDATE SYS_GOINFO SET FROMSTATION=:fromStation,TRAINNUMBER=:trainNumber,FROMDATE=:fromDate,EXITPORTSTATION=:exitPortStation,OVERSEASSTATION=:overseasStation,OVERSEASCOUNTRY=:overseasCountry,OVERSEASCITY=:overseasCity,TRAINSTATE:=trainState,UPDATETIME=:updateTime,TRAINQTY=:trainQty,CARRIAGEQTY=:carriageQty,HEAVYQTYTWENTY=:HeavyQtyTwenty,EMPTYQTYTWENTY=:EmptyQtyTwenty,HEAVYQTYforty=:HeavyQtyForty,EMPTYQTYFORTY=:EmptyQtyForty,HEAVYQTYFORTYFIVE=:HeavyQtyFortyfive,EMPTYQTYFORTYFIVE=:EmptyQtyFortyfive,TEU=:TEU,COLDTEU=:coldTEU,COLDWEIGHT=:coldWeight,REMARK=:remark,ISDELETE=:isDelete,SAVETYPE=:saveType";
         NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
         SqlParameterSource paramSource = new BeanPropertySqlParameterSource(goInfo);
         return namedParameterJdbcTemplate.update(sql, paramSource);
