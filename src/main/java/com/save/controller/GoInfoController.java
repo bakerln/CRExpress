106b16acb03de751b82bb49a7f54305d9f378787
renxinliang <451757560@qq.com>
2018-02-08 15:51:23 +0800

    from ren

--- src/main/java/com/save/controller/GoInfoController.java
+++ src/main/java/com/save/controller/GoInfoController.java
@@ -1,6 +1,6 @@
 package com.save.controller;
 
-import com.alibaba.druid.wall.violation.ErrorCode;
+
 import com.common.util.json.JsonUtil;
 import com.common.util.json.ResultMsg;
 import com.common.util.session.SessionUtil;
@@ -8,7 +8,7 @@ import com.common.util.session.UserSession;
 import com.common.util.web.WebUtil;
 import com.save.model.GoInfo;
 import com.save.service.GoInfoService;
-import com.sys.model.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +32,7 @@ public class GoInfoController {
      *
      * @param request
      * @param response
-     * @param goInfo   //去程信息
+     * @param goInfo
      */
     @RequestMapping(value = "/add")
     public void add(HttpServletRequest request, HttpServletResponse response, GoInfo goInfo) {
@@ -49,44 +49,83 @@ public class GoInfoController {
 
     /**
      * 修改信息
+     *
      * @param request
      * @param response
-     * @param goInfo   //去程信息
+     * @param goInfo
      */
     @RequestMapping(value = "/update")
     public void update(HttpServletRequest request, HttpServletResponse response, GoInfo goInfo) {
         UserSession userSession = SessionUtil.getUserSession(request);
         ResultMsg resultMsg = goInfoService.update(goInfo, userSession);
-        if (0 == resultMsg.getErrcode()){
+        if (0 == resultMsg.getErrcode()) {
             WebUtil.out(response, JsonUtil.createOperaStr(true, "修改成功"));
-        }else if (1 == resultMsg.getErrcode()){
+        } else if (1 == resultMsg.getErrcode()) {
             WebUtil.out(response, JsonUtil.createOperaStr(true, "请重新登录"));
-        }else if (2 == resultMsg.getErrcode()){
+        } else if (2 == resultMsg.getErrcode()) {
             WebUtil.out(response, JsonUtil.createOperaStr(true, "已提交信息不允许修改"));
-        }else {
+        } else {
             WebUtil.out(response, JsonUtil.createOperaStr(true, "修改失败"));
+        }
+    }
+
+    /**
+     * 删除信息
+     *
+     * @param request
+     * @param response
+     * @param goInfo
+     */
+
+    @RequestMapping(value = "/delete")
+    public void delete(HttpServletRequest request, HttpServletResponse response, GoInfo goInfo) {
+        UserSession userSession = SessionUtil.getUserSession(request);
+        ResultMsg resultMsg = goInfoService.delete(goInfo, userSession);
+        if (0 == resultMsg.getErrcode()) {
+            WebUtil.out(response, JsonUtil.createOperaStr(true, "删除成功"));
+        } else if (1 == resultMsg.getErrcode()) {
+            WebUtil.out(response, JsonUtil.createOperaStr(true, "未取得登录信息，请重新登录"));
+        } else if (2 == resultMsg.getErrcode()) {
+            WebUtil.out(response, JsonUtil.createOperaStr(true, "已提交的数据不可删除"));
+        } else {
+            WebUtil.out(response, JsonUtil.createOperaStr(true, "删除失败"));
+        }
+    }
+
+    /**
+     * @param request
+     * @param response
+     * @param goInfo
+     */
+    @RequestMapping(value = "/list")
+    public void listGoinfo(HttpServletRequest request, HttpServletResponse response, GoInfo goInfo) {
+        UserSession userSession = SessionUtil.getUserSession(request);
+        int count = goInfoService.listCount(goInfo,userSession);
+        String json = JsonUtil.createPageJson(count, goInfoService.listGoinfo(goInfo,userSession));
+        WebUtil.out(response, json);
+    }
+
+
+    /**
+     * @param request
+     * @param response
+     * @param goInfo
+     */
+    @RequestMapping(value = "/submit")
+    public void submit(HttpServletRequest request, HttpServletResponse response, GoInfo goInfo) {
+        UserSession userSession = SessionUtil.getUserSession(request);
+        ResultMsg resultMsg = goInfoService.submit(goInfo, userSession);
+        if (0 == resultMsg.getErrcode()) {
+            WebUtil.out(response, JsonUtil.createOperaStr(true, "提交成功"));
+        } else if (1 == resultMsg.getErrcode()) {
+            WebUtil.out(response, JsonUtil.createOperaStr(true, "未取得登录信息，请重新登录"));
+        } else if (2 == resultMsg.getErrcode()) {
+            WebUtil.out(response, JsonUtil.createOperaStr(true, "已提交的数据不可再次提交"));
+        } else {
+            WebUtil.out(response, JsonUtil.createOperaStr(true, "提交失败"));
         }
     }
 
 
 
-    @RequestMapping(value = "/delete")
-    public void delete() {
-
-    }
-
-    @RequestMapping(value = "/list")
-    public void list() {
-
-    }
-
-    @RequestMapping(value = "/weekSubmit")
-    public void weekSubmit() {
-
-    }
-
-    @RequestMapping(value = "/monthSubmit")
-    public void monthSubmit() {
-
-    }
 }
