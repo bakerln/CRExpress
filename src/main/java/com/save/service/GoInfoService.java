6ab58f21747f92c4c32ffd0a88bb97043c99c9db
renxinliang <451757560@qq.com>
2018-02-23 09:17:26 +0800

    goinfoService

--- src/main/java/com/save/service/GoInfoService.java
+++ src/main/java/com/save/service/GoInfoService.java
@@ -9,7 +9,7 @@ import com.sun.org.apache.xalan.internal.xsltc.compiler.util.ErrorMsg;
 import com.sys.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.save.dto.GoInfoVO;
 import java.util.Date;
 import java.util.List;
 
@@ -23,22 +23,29 @@ public class GoInfoService {
     @Autowired
     private GoInfoDao goInfoDao;
 
+    //得到id
+    public String getId(GoInfoVO goInfoVO) {
+        int id = goInfoDao.createTrainId();
+        String date = goInfoDao.createDate();
+        int flag = goInfoVO.getTrainType();
+        String idString = date + flag + id;
+        System.out.println(idString);
+        return idString;
+    }
     //新增信息
-    public ResultMsg add(GoInfo goInfo, UserSession userSession) {
+    public ResultMsg add(GoInfoVO goInfoVO, UserSession userSession) {
         if (userSession != null) {
                //判断用户角色
             if (1 == userSession.getRoleId()) {
                 return new ResultMsg(2, "该用户不允许添加信息", null);
             }
-            else {
-                int goInfoID = goInfoDao.createGOInfoId();
-                goInfo.setId(goInfoID);//添加信息ID
-                goInfo.setCreateTime(new Date());//添加创建时间
-                goInfo.setUserID(userSession.getUserId());//添加创建用户ID
-                goInfo.setIsDelete(1);//设置删除状态为“可用”
-                goInfo.setSaveType(1);//设置存储状态为“暂存”
-                goInfo.setOrgID(userSession.getOrgId());//添加创建人所属单位
-                goInfoDao.add(goInfo);
+            else{
+                goInfoVO.setId(getId(goInfoVO));//添加信息ID
+                goInfoVO.setCreateTime(new Date());//添加创建时间
+                goInfoVO.setUserID(userSession.getUserId());//添加创建用户ID
+                goInfoVO.setStatus(1);//设置状态为“暂存”
+                goInfoVO.setOrgID(userSession.getOrgId());//添加创建人所属单位
+                goInfoDao.add(goInfoVO);
             }
             //判断数据合法性 数据合法性规则未知
         } else {
@@ -48,14 +55,14 @@ public class GoInfoService {
     }
 
      //修改信息
-     public ResultMsg update(GoInfo goInfo, UserSession userSession) {
+     public ResultMsg update(GoInfoVO goInfoVO, UserSession userSession) {
          if (userSession != null) {
              //判断存储状态
-             if(2 == goInfo.getSaveType()){
+             if(2 == goInfoVO.getStatus()){
                  return new ResultMsg(2,"已提交信息不允许修改",null);
              } else {
-                 goInfo.setUpdateTime(new Date());//添加更新时间
-                 goInfoDao.update(goInfo);
+                 goInfoVO.setUpdateTime(new Date());//添加更新时间
+                 goInfoDao.update(goInfoVO);
              }
          } else {
              return new ResultMsg(1, "未取得登录信息", null);
@@ -63,15 +70,16 @@ public class GoInfoService {
          return new ResultMsg(0, "修改成功", null);
      }
 
+
    //删除信息
-   public ResultMsg delete(GoInfo goInfo, UserSession userSession) {
+   public ResultMsg delete(GoInfoVO goInfoVO, UserSession userSession) {
        if (userSession != null) {
            //判断存储状态
-           if (2 == goInfo.getSaveType()) {
+           if (2 == goInfoVO.getStatus()) {
                return new ResultMsg(2, "已提交信息不允许删除", null);
            } else {
-               goInfo.setIsDelete(2);//设置状态为删除
-               goInfoDao.delete(goInfo);
+               goInfoVO.setStatus(3);//设置状态为删除
+               goInfoDao.delete(goInfoVO);
            }
        } else {
            return new ResultMsg(1, "未取得登录信息", null);
@@ -80,24 +88,23 @@ public class GoInfoService {
    }
 
     //信息展示
-    public int listCount(GoInfo goInfo,UserSession userSession) {
-        return goInfoDao.listCount(goInfo,userSession);
+    public int listCount(GoInfoVO goInfoVO,UserSession userSession) {
+        return goInfoDao.listCount(goInfoVO,userSession);
     }
-
-    public List<GoInfo> listGoinfo(GoInfo goInfo,UserSession userSession) {
-        List<GoInfo> goInfoList = goInfoDao.listGoinfo(goInfo,userSession);
+    public List<GoInfoVO> listGoinfo(GoInfoVO goInfoVO,UserSession userSession) {
+        List<GoInfoVO> goInfoList = goInfoDao.listGoinfo(goInfoVO,userSession);
         return goInfoList;
     }
 
     //提交信息
-    public ResultMsg submit(GoInfo goInfo, UserSession userSession) {
+    public ResultMsg submit(GoInfoVO goInfoVO, UserSession userSession) {
         if (userSession != null) {
             //判断是否删除
-            if (2 == goInfo.getSaveType()) {
+            if (2 == goInfoVO.getStatus()) {
                 return new ResultMsg(2, "已提交信息不允许再次提交", null);
             } else {
-                goInfo.setSaveType(2);//设置存储状态为提交
-                goInfoDao.submit(goInfo);
+                goInfoVO.setStatus(2);//设置存储状态为提交
+                goInfoDao.submit(goInfoVO);
             }
         } else {
             return new ResultMsg(1, "未取得登录信息", null);
@@ -105,13 +112,6 @@ public class GoInfoService {
         return new ResultMsg(0, "提交成功", null);
     }
 
-//    //得到id
-//    public void getId(String[] args) {
-//        int id = goInfoDao.createTrainId();
-//        String date = goInfoDao.createDate();
-//        String flag = "1";//中欧1、中亚2
-//        String idString = date + flag + id;
-//        System.out.println(idString);
-//    }
+
 }
 
